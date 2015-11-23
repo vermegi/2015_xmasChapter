9e6aee8e6362c450dbe1ac7202b1ec15218dbd5e
vermegi <gitte.vermeiren@gmail.com>
2015-11-23 14:01:22 +0100

    gulp tasks for copying bower files

---
+++ src/EventPlanner/EventPlanner/gulpfile.js
@@ -0,0 +1,27 @@
+﻿var gulp = require('gulp'),
+    del = require('del'),
+    bower = require('gulp-bower'),
+    mainBowerFiles = require('main-bower-files'),
+    changed = require('gulp-changed');
+
+var paths = {
+    bower: "./bower_components/",
+    npm: "./node_modules/",
+    content: "./Content/",
+    lib: "./Scripts/lib/",
+    app: "./Scripts/app/",
+    the_app: "./Scripts/app/the_app/",
+    css: "./Content/css/"
+};
+
+gulp.task('bower-install', function () {
+    return bower({ cmd: 'install' });
+});
+
+gulp.task('bower-files', ['bower-install'], function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(changed(paths.lib))
+        .pipe(gulp.dest(paths.lib));
+});
+
+gulp.task('default', ['bower-files'], function () { });
