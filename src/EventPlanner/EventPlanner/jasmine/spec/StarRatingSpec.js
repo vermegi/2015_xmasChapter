6dba7e3b0a452c029c83dc2272e5b69c220c4269
vermegi <gitte.vermeiren@gmail.com>
2016-01-19 19:20:13 +0100

    more expectations

--- src/EventPlanner/EventPlanner/jasmine/spec/StarRatingSpec.js
+++ src/EventPlanner/EventPlanner/jasmine/spec/StarRatingSpec.js
@@ -1,13 +1,56 @@
 ﻿describe('star rating', function () {
 
-    var starRating;
+    var starRating, ratingServiceSpy;
     beforeEach(function() {
-        starRating = new StarRating('<div>');
+        ratingServiceSpy = {
+            addRating: function () { }
+        };
+
+        starRating = new StarRating('<div>', ratingServiceSpy);
         starRating.render();
     });
 
     it('initially sets all stars as empty', function() {
         expect(starRating.el.find('.star').length).toBe(3);
         expect(starRating.el.find('.filled').length).toBe(0);
+    });
+
+    describe('clicking on the first star', function () {
+
+        beforeEach(function () {
+            spyOn(ratingServiceSpy, 'addRating')
+                .and.callFake(function(req) {
+                    var d = $.Deferred();
+                    d.resolve(1);
+                    return d.promise();
+                });
+            starRating.el.find('.star').eq(0).trigger('click');
+        });
+
+        it('will make a call to the rating service', function() {
+            expect(ratingServiceSpy.addRating).toHaveBeenCalledWith(1);
+        });
+
+        it('fills one star', function () {
+            expect(starRating.el.find('.star').length).toBe(3);
+            expect(starRating.el.find('.filled').length).toBe(1);
+        });
+    });
+
+    describe('clicking on the second star', function () {
+
+        beforeEach(function () {
+            spyOn(ratingServiceSpy, 'addRating')
+                .and.callFake(function (req) {
+                    var d = $.Deferred();
+                    d.resolve(2);
+                    return d.promise();
+                });
+            starRating.el.find('.star').eq(1).trigger('click');
+        });
+
+        it('will make a call to the rating service', function() {
+            expect(ratingServiceSpy.addRating).toHaveBeenCalledWith(2);
+        });
     });
 });
--- src/EventPlanner/EventPlanner/jasmine/src/StarRating.js
+++ src/EventPlanner/EventPlanner/jasmine/src/StarRating.js
@@ -1,10 +1,29 @@
-﻿var StarRating = function(el) {
+﻿var StarRating = function(el, service) {
     this.el = $(el);
+    this.service = service;
 };
 
 StarRating.prototype.render = function () {
-    var template = '<span class="star"></span>' +
-        '<span class="star"></span>' +
-        '<span class="star"></span>';
+    var _this = this;
+    var template = '<span class="star" data-rating="1"></span>' +
+        '<span class="star" data-rating="2"></span>' +
+        '<span class="star" data-rating="3"></span>';
     this.el.append($(template));
+
+    this.el.children().each(function() {
+        $(this).on('click', $.proxy(_this.starIt, _this));
+    });
 };
+
+StarRating.prototype.starIt = function (event) {
+    var _this = this;
+    var data = parseInt($(event.currentTarget).data('rating'), 10);
+    this.service.addRating(data)
+        .done($.proxy(_this.updateStars, _this));
+};
+
+StarRating.prototype.updateStars = function(data) {
+    for (var i = 0; i < data; i++) {
+        this.el.children().eq(i).addClass('filled');
+    }
+}; 
