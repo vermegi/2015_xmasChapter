675202c9f9e7410a7edd66ccd090978a8474b474
vermegi <gitte.vermeiren@gmail.com>
2016-01-19 18:34:36 +0100

    added first base test case

---
+++ src/EventPlanner/EventPlanner/jasmine/spec/StarRatingSpec.js
@@ -0,0 +1,13 @@
+﻿describe('star rating', function () {
+
+    var starRating;
+    beforeEach(function() {
+        starRating = new StarRating('<div>');
+        starRating.render();
+    });
+
+    it('initially sets all stars as empty', function() {
+        expect(starRating.el.find('.star').length).toBe(3);
+        expect(starRating.el.find('.filled').length).toBe(0);
+    });
+});
---
+++ src/EventPlanner/EventPlanner/jasmine/src/StarRating.js
@@ -0,0 +1,10 @@
+﻿var StarRating = function(el) {
+    this.el = $(el);
+};
+
+StarRating.prototype.render = function () {
+    var template = '<span class="star"></span>' +
+        '<span class="star"></span>' +
+        '<span class="star"></span>';
+    this.el.append($(template));
+};
