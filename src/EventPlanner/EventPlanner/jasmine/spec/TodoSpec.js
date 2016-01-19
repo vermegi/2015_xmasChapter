cd21abdcac226585f31b15dd165aa946e518aa51
vermegi <gitte.vermeiren@gmail.com>
2016-01-19 20:01:48 +0100

    uitgebreidere test

--- src/EventPlanner/EventPlanner/jasmine/spec/TodoSpec.js
+++ src/EventPlanner/EventPlanner/jasmine/spec/TodoSpec.js
@@ -1,5 +1,10 @@
 ﻿function TodoVM() {
     this.todos = ko.observableArray();
+    this.currentTodo = ko.observable();
+};
+
+TodoVM.prototype.addTodo = function() {
+    this.todos.push(this.currentTodo());
 };
 
 describe('TodoViewModel', function () {
@@ -10,8 +15,19 @@ describe('TodoViewModel', function () {
     });
 
     it('has an empty list of todos', function() {
-        expect(vm.todos.length).toBe(0);
+        expect(vm.todos().length).toBe(0);
         expect(ko.isObservable(vm.todos)).toBeTruthy();
     });
 
+    describe('adding a todo item', function() {
+
+        beforeEach(function() {
+            vm.currentTodo("do something");
+            vm.addTodo();
+        });
+
+        it('adds the current todo to the list of todos', function() {
+            expect(vm.todos().length).toBe(1);
+        });
+    });
 });
